d8f2cc135db0e8ec439ba6a1c1398edcebe18385
Noah <noahburnham23@gmail.com>
2025-06-24 16:52:04 -0400

    added (home) router, navbar, navbar sidebar, and footer.

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return(
+        <footer className="flex border-t justify-between font-medium p-6">
+            <p className="flex items-center gap-2">Locality LLC 2025</p>
+        </footer>
+    );
+};
