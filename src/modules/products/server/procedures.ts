45893ce4e48eca8bcc1ecc82b67f52f6643f15e1
Noah <noahburnham23@gmail.com>
2025-11-03 10:34:41 -0500

    12 filtering products fix

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -18,12 +18,14 @@ export const productsRouter = createTRPCRouter({
 
       if(input.maxPrice){
         where.price = {
+          ...where.price,
           less_than_equal: input.maxPrice
         }
       }
 
       if(input.minPrice){
         where.price = {
+          ...where.price,
           greater_than_equal: input.minPrice
         }
       }
