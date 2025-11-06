41630bf00052f71ce0d54b005a94eaf3b462f1f7
Noah <noahburnham23@gmail.com>
2025-11-05 23:00:16 -0500

    15 multi-tenancy

--- src/modules/home/ui/components/search-filters/category-dropdown.tsx
+++ src/modules/home/ui/components/search-filters/category-dropdown.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useRef, useState } from "react";
-import { UseDropdownPosition } from "./use-dropdown-position";
 import { SubcategoryMenu } from "./subcategory-menu";
 import Link from "next/link";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
@@ -21,7 +20,6 @@ export const CategoryDropdown = ({
 }: Props) => {
     const [isOpen, setIsOpen] = useState(false);
     const dropdownRef = useRef<HTMLDivElement>(null);
-    const { getDropdownPosition } = UseDropdownPosition(dropdownRef);
 
     const onMouseEnter = () => {
         if(category.subcategories){
@@ -29,7 +27,6 @@ export const CategoryDropdown = ({
         }
     }
     const onMouseLeave = () => setIsOpen(false);
-    const dropdownPosition = getDropdownPosition();
     // TODO: potentially improve mobile
     // const toggleDropdown = () => {
     //     if(category.subcategories?.docs?.length){
@@ -71,7 +68,6 @@ export const CategoryDropdown = ({
             <SubcategoryMenu 
                 category={category}
                 isOpen={isOpen}
-                position={dropdownPosition}
             />
         </div>
     );
