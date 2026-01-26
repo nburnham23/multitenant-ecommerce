938a4c350f9c9621b86e0c4a153684ca78e647bc
Noah <noahburnham23@gmail.com>
2026-01-25 21:25:14 -0500

    19 checkout page

---
+++ src/modules/checkout/server/procedures.ts
@@ -0,0 +1,44 @@
+import z from "zod";
+
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Media, Tenant } from "@/payload-types";
+import { TRPCError } from "@trpc/server";
+
+export const checkoutRouter = createTRPCRouter({
+  getProducts: baseProcedure
+    .input(
+        z.object({
+          ids: z.array(z.string()),
+    }),
+  )
+    .query(async({ ctx, input }) => {
+      const data = await ctx.db.find({
+        collection: "products",
+        depth: 2, // Populate "category", "image", "tenant", & "tenant.image"
+        where: {
+          id: {
+            in: input.ids,
+          },
+        },
+      });
+
+      if(data.totalDocs !== input.ids.length){
+        throw new TRPCError({ code: "NOT_FOUND", message: "products not found"});
+      }
+
+      const totalPrice = data.docs.reduce((acc, product) => {
+        const price = Number(product.price);
+        return acc + (isNaN(product.price) ? 0 : price);
+      }, 0);
+          
+      return {
+        ...data,
+        totalPrice: totalPrice,
+        docs: data.docs.map((doc) => ({
+          ...doc,
+          image: doc.image as Media | null,
+          tenant: doc.tenant as Tenant & { image: Media | null },
+        }))
+      };
+    }),
+});
