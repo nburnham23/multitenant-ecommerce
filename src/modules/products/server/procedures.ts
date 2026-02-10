75c5ea6b457b7e97af7de52d6c51f785e5231213
Noah <noahburnham23@gmail.com>
2026-02-09 23:28:19 -0500

    21: library

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -5,6 +5,7 @@ import type { Sort, Where } from "payload";
 import { Category, Media, Tenant } from "@/payload-types";
 import { sortValues } from "../search-params";
 import { DEFAULT_LIMIT } from "@/constants";
+import { headers as getHeaders } from "next/headers";
 
 export const productsRouter = createTRPCRouter({
   getOne: baseProcedure
@@ -14,14 +15,43 @@ export const productsRouter = createTRPCRouter({
       })
     )
     .query(async ({ctx, input}) => {
+      const headers = await getHeaders();
+      const session = await ctx.db.auth({ headers });
       const product = await ctx.db.findByID({
         collection: "products",
         id: input.id,
         depth: 2, // load product.image, product.tenant, and product.tenant.image
       });
 
+      let isPurchased = false;
+
+      if(session.user){
+        const ordersData = await ctx.db.find({
+          collection: "orders",
+          pagination: false,
+          limit: 1,
+          where: {
+            and: [
+              {
+                product: {
+                  equals: input.id,
+                },
+              },
+              {
+                user: {
+                  equals: session.user.id,
+                },
+              },
+            ],
+          },
+        });
+
+        isPurchased = !!ordersData.docs[0];
+      }
+
       return {
         ...product,
+        isPurchased,
         image: product.image as Media | null,
         tenant: product.tenant as Tenant & { image: Media | null }
       };
