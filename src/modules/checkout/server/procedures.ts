754626351acb419b97cfb5f2d2b25f9afb5d3a68
Noah <noahburnham23@gmail.com>
2026-02-23 21:57:58 -0500

    25: stripe connect

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -6,8 +6,54 @@ import { TRPCError } from "@trpc/server";
 import type Stripe from "stripe";
 import { CheckoutMetadata, ProductMetadata } from "../types";
 import { stripe } from "@/lib/stripe";
+import { PLATFORM_FEE_PERCENTAGE } from "@/constants";
 
 export const checkoutRouter = createTRPCRouter({
+  verify: protectedProcedure
+    .mutation(async ({ ctx }) => {
+      const user = await ctx.db.findByID({
+        collection: "users",
+        id: ctx.session.user.id,
+        depth: 0,
+      });
+
+      if(!user){
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "User not found",
+        });
+      }
+
+      const tenantId = user.tenants?.[0]?.tenant as string; // this is an id because depth = 0
+      const tenant = await ctx.db.findByID({
+        collection: "tenants",
+        id: tenantId,
+      });
+
+      if(!tenant){
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Tenant not found",
+        });
+      }
+
+      const accountLink = await stripe.accountLinks.create({
+        account: tenant.stripeAccountId,
+        refresh_url: `${process.env.NEXT_PUBLIC_APP_URL!}/admin`,
+        return_url: `${process.env.NEXT_PUBLIC_APP_URL!}/admin`,
+        type: "account_onboarding",
+      });
+
+      if(!accountLink.url){
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Failed to create verification link",
+        });
+      }
+
+      return { url: accountLink.url };
+
+    }),
   purchase: protectedProcedure
     .input(
       z.object({
@@ -52,10 +98,18 @@ export const checkoutRouter = createTRPCRouter({
       const tenant = tenantsData.docs[0];
 
       if (!tenant) {
-        throw new TRPCError({ code: "NOT_FOUND", message: "Tenant not found"})
+        throw new TRPCError({ 
+          code: "NOT_FOUND", 
+          message: "Tenant not found"
+        })
+      }
+      if (!tenant.stripeDetailsSubmitted) {
+        throw new TRPCError({ 
+          code: "BAD_REQUEST", 
+          message: "Tenant not allowed to sell products."
+        })
       }
 
-      // TODO: Throw error if Stripe details not submitted
 
       const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = 
         products.docs.map((product) => ({
@@ -74,6 +128,13 @@ export const checkoutRouter = createTRPCRouter({
             }
           }
         }));
+
+      const totalAmount = products.docs.reduce(
+        (acc, item) => acc + item.price * 100,
+        0
+      );
+
+      const platformFeeAmount = Math.round(totalAmount * (PLATFORM_FEE_PERCENTAGE / 100));
       
       const checkout = await stripe.checkout.sessions.create({
         customer_email: ctx.session.user.email,
@@ -86,7 +147,12 @@ export const checkoutRouter = createTRPCRouter({
         },
         metadata: {
           userId: ctx.session.user.id,
-        } as CheckoutMetadata
+        } as CheckoutMetadata,
+        payment_intent_data: { // to get rid of fee percentage, can get rid of what's in these braces
+          application_fee_amount: platformFeeAmount,
+        },
+      }, { // and may need to get rid of what's in these braces too
+        stripeAccount: tenant.stripeAccountId,
       });
 
       if (!checkout.url){
--- src/seed.ts
+++ src/seed.ts
@@ -1,5 +1,6 @@
 import { getPayload } from "payload"
 import config from "@payload-config"
+import { stripe } from "./lib/stripe";
 
 const categories = [
     {
@@ -37,13 +38,16 @@ const categories = [
 
   const seed = async () => {
     const payload = await getPayload({ config });
+
+    const adminAccount = await stripe.accounts.create({});
+
     // create admin tenant
     const adminTenant = await payload.create({
       collection: "tenants",
       data: {
         name: "admin",
         slug: "admin",
-        stripeAccountId: "admin",
+        stripeAccountId: adminAccount.id,
       },
     });
 
